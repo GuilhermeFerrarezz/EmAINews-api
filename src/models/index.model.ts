2599576c1c5eb139daadc3947daa40a783168771
Guilherme <guifcunha22@gmail.com>
2026-02-19 20:50:05 -0300

    feat: define Database interface and type for sequelize and User model Co-authored-by: Guilherme Omar Silva guilherme.omars@gmail.com

--- src/models/index.model.ts
+++ src/models/index.model.ts
@@ -1,8 +1,12 @@
 import User from './user.Model.js';
 import sequelize from "../config/database.js";
+import type { Sequelize } from 'sequelize';
+interface Database {
+    sequelize: Sequelize;
+    User: typeof User; 
+}
 
-
-const db = {
+const db: Database= {
     sequelize,
     User,
 }
